d88bc264196e24275012648f8ffc03dfd300fb9c
Narek Avagyan <ava.narek@gmail.com>
2020-03-13 01:32:00 +0300

    Update TwitterApi.js

--- server/TwitterApi.js
+++ server/TwitterApi.js
@@ -40,9 +40,15 @@ async function getTweetsBySearch(searchString, socket) {
     } catch (e) {}
   };
 
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+  });
 
   const page = await browser.newPage();
+  const client = await page.target().createCDPSession();
+  await client.send('Network.clearBrowserCookies');
+  await client.send('Network.clearBrowserCache');
+  
   await page.setUserAgent(
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
   );
